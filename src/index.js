fe0c42fa3a5aed55c9ef71ec6210db358e2c3ca8
brewchetta <chett.tiller@gmail.com>
2018-11-23 20:33:40 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //YOUR CODE HERE
+  const pokemonContainer = document.getElementById('pokemon-container')
+
+  // Builds the pokemon
+  POKEMON.forEach((poke) => {
+    const pokeBox = document.createElement('div')
+    pokeBox.innerHTML = `<p>${poke['name']}</p> <br> <img src='${poke["sprites"]["front"]}'>`
+    pokeBox.classList.add(`pokemon-container`)
+    pokemonContainer.appendChild(pokeBox)
+
+    // Adds click event
+    pokeBox.addEventListener('click', () => {
+      if (pokeBox.classList.contains('back')) {
+        pokeBox.querySelector('img').src = poke['sprites']['front']
+        pokeBox.classList.remove('back')
+      } else {
+        pokeBox.querySelector('img').src = poke['sprites']['back']
+        pokeBox.classList.add('back')
+      }
+    })
+  })
+
+  const pokemonSearchInput = document.getElementById('pokemon-search-input')
+  pokemonSearchInput.addEventListener('input', (e) => {
+    e.preventDefault()
+    const pokemonContainer = document.getElementById('pokemon-container')
+    let pokeArray = Array.from(pokemonContainer.getElementsByClassName('pokemon-container'))
+
+    pokeArray.forEach((poke) => {
+      const name = poke.querySelector('p').innerText
+      if (!name.toLowerCase().includes(pokemonSearchInput.value.toLowerCase())) {
+        poke.classList.add('hide')
+      } else {
+        poke.classList.remove('hide')
+      }
+    })
+  })
 })
